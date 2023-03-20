217cfc7effde23b6861aed39ad91b78cfa556790
mehulrastogi12 <61135464+mehulrastogi12@users.noreply.github.com>
2023-03-20 22:44:11 +0530

    Delete Main.scala

--- src/main/scala/Mehul/Main.scala
+++
@@ -1,8 +0,0 @@
-package Mehul
-//import Mehul.Lastindex.List_last_index.add
-import Mehul.List_last.list_recursion
-object Main {
-  //println("Welcome to Scala")
-  assert(list_recursion(List(2,3,4,5))==5,"wrong answer")
-
-}
