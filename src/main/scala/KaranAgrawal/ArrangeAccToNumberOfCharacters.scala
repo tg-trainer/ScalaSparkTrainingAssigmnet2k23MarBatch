c6269616916d963581973885c9e3226bee57185f
Ayush Tiwari <127586942+tg-trainer@users.noreply.github.com>
2023-04-03 15:21:28 +0530

    Revert "Question11"

--- src/main/scala/KaranAgrawal/ArrangeAccToNumberOfCharacters.scala
+++
@@ -1,9 +0,0 @@
-package KaranAgrawal
-
-object ArrangeAccToNumberOfCharacters extends App {
-  val list=List("hello","hi","hey","welcome","great","nice")
-  def arrangeAccToNumberOfCharacters(list:List[String]):List[String]={
-    list.sortBy(_.length)
-  }
-  println(arrangeAccToNumberOfCharacters(list))
-}
